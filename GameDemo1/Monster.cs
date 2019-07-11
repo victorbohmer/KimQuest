3a142e776f6ef4c8c95b47a74357025defdc77ea
SweetDude8 <linus_ekblom@live.se>
2019-07-11 14:26:30 +0200

    Added monster, Player and Combat

---
+++ GameDemo1/Monster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniProjektGame
+{
+    class Monster
+    {
+        public int Health { get; private set; }
+        private int minDamage;
+        private int maxDamage;
+
+        private int minGold;
+        private int maxGold;
+        public Monster()
+        {
+            minDamage = 10;
+            maxDamage = 20;
+            Health = 100;
+        }
+        public int DropGold()
+        {
+            Random random = new Random();
+            return random.Next(minGold, maxGold);
+        }
+        public int DoDamage()
+        {
+            Random random = new Random();
+            return random.Next(minDamage, maxDamage);
+        }
+    }
+}
---
+++ GameDemo1/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniProjektGame
+{
+    class Player
+    {
+        public int Health { get; private set; }
+        public int Gold { get; private set; }
+
+
+        private int minDamage;
+        private int maxDamage;
+        public Player()
+        {
+            minDamage = 10;
+            maxDamage = 25;
+            Health = 100;
+            Gold = 10;
+        }
+        public int DoDamage()
+        {
+            Random random = new Random();
+            return random.Next(minDamage, maxDamage);
+        }
+
+
+
+    }
+}
